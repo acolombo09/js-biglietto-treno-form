b74f39f42f5ddd1ac63668be8b034890a9db410c
acolombo09 <acolombo0911@gmail.com>
2023-07-03 15:56:35 +0200

    exercise_starting_brainstorming

--- js/main.js
+++ js/main.js
@@ -1,14 +1,36 @@
 /*
-Il programma dovrà chiedere all’utente il numero di chilometri 
-che vuole percorrere e l’età del passeggero.
-Sulla base di queste informazioni dovrà calcolare il prezzo totale 
-del viaggio, secondo queste regole:
-il prezzo del biglietto è definito in base ai km (0.21 € al km)
-va applicato uno sconto del 20% per i minorenni
-va applicato uno sconto del 40% per gli over 65.
-L’output del prezzo finale va messo fuori in forma umana 
-(con massimo due decimali, per indicare centesimi sul prezzo). 
-Questo richiederà un minimo di ricerca.
+Descrizione:
+Scrivere un programma che chieda all’utente:
+- Il numero di chilometri da percorrere
+- Età del passeggero
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto 
+di viaggio, secondo le seguenti regole:
+- il prezzo del biglietto è definito in base ai km (0.21 € al km)
+- va applicato uno sconto del 20% per i minorenni
+- va applicato uno sconto del 40% per gli over 65.
+
+MILESTONE 1:
+Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente 
+due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. 
+La risposta finale (o output) sarà anch’essa da scrivere in console.
+
+MILESTONE 2:
+Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo 
+un form in pagina in cui l’utente potrà inserire i dati e visualizzare 
+il calcolo finale con il prezzo.
+Il recap dei dati e l’output del prezzo finale, andranno quindi stampati in 
+pagina (il prezzo dovrà essere formattato con massimo due decimali, 
+per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+Nota:
+Se non vi sentite particolarmente creativi, questa potrebbe essere 
+un’implementazione da seguire per il secondo milestone. 
+Potete scegliere di implementare una soluzione completamente diversa oppure simile,
+ma in ogni caso cercate di farla vostra.
+
+Bonus:
+al click su annulla, si svuotano i campi e scopare il biglietto
+usare una select per far scegliere la fascia d’età all’utente
 */
 
 const firstName = prompt("Inserisci il tuo nome");
@@ -22,37 +44,27 @@ document.getElementById("last-name").innerHTML =  lastName;
 document.getElementById("age").innerHTML = age;
 document.getElementById("req-kms").innerHTML = reqKms;
 
-//To fixed applica in automatico le approssimazioni, che sia floor o ceil.
 const priceKms = (reqKms * pricePerKm).toFixed(2);
 
-// console.log(priceKms + " euro");
 document.getElementById("price-kms").innerHTML = priceKms + " €";
 
-/* Come miglioria, si potrebbe dare al let coupon un =0 per 
-  evitare di aggiungere un else nell'if per gli sconti.
-*/
-const discountBaby = 20;
-const discountOver = 40;
-let coupon;
+let coupon = 0;
 
 if (age < 18) {
-    document.getElementById("discount-calc").innerHTML = (discountBaby + " %");
-  } else if (age > 65){
-    document.getElementById("discount-calc").innerHTML = (discountOver + " %");
-  } else {
-    document.getElementById("discount-calc").innerHTML = "No discounts applied";
-}
-
-if (age < 18) {
-    coupon = ((priceKms * discountBaby) / 100);
+    coupon = 20;
   } else if (age > 65) {
-    coupon = ((priceKms * discountOver) / 100);
-  } else {
-    coupon = 0;
+    coupon = 40;
 }
 
-// console.log((priceKms * discountBaby / 100));
-// console.log((priceKms * discountOver / 100));
+if (age < 18) {
+  document.getElementById("discount-calc").innerHTML = (coupon + " %");
+} else if (age > 65){
+  document.getElementById("discount-calc").innerHTML = (coupon + " %");
+} else {
+  document.getElementById("discount-calc").innerHTML = "No discounts applied";
+}
 
-const finalPrice = (priceKms - coupon);
+let discountAmount = (priceKms * coupon) / 100;
+
+const finalPrice = (priceKms - discountAmount);
 document.getElementById("final-price").innerHTML = finalPrice + " €";
